8a23864171f58bbd43ac4b965da51eb4efb6c1d8
mabus <m.buscaroli@outlook.com>
2022-03-23 15:22:15 +0000

    added patch operation and test

--- src/routers/user.js
+++ src/routers/user.js
@@ -24,4 +24,27 @@ router.delete('/users/:id', async (req, res) => {
   }
 })
 
+router.patch('/users/:id', async (req, res) => {
+  const id = await req.params.id
+
+  // Check all updates are allowed (eg updating id or timestamp disallowed)
+  const allowedUpdates = ['name', 'email', 'password']
+  const updates = Object.keys(req.body)
+  const isValid = updates.every((prop) => allowedUpdates.includes(prop))
+
+  if (!isValid) {
+    return res.status(400).send({ error: 'Opration not allowed.' })
+  }
+
+  try {
+    // find the user by ID and update its details
+    let newUser = await User.findByIdAndUpdate(id, req.body)
+
+    // console.log(newUser)
+    res.status(200).send(newUser)
+  } catch (err) {
+    throw new Error(err)
+  }
+})
+
 module.exports = router
--- tests/user.test.js
+++ tests/user.test.js
@@ -6,47 +6,46 @@ const User = require('../src/models/user')
 beforeEach(async () => await User.deleteMany())
 afterAll(async () => await mongoose.connection.close())
 
-const matt = {
+const userOne = {
   name: 'Matt',
   email: 'matt@email.com',
   password: 'mattpassword',
 }
 
-const john = {
+const userTwo = {
   name: 'John',
   email: 'john@email.com',
   password: 'johnpassword',
 }
 
+const userThree = {
+  name: 'Susan',
+  email: 'susan@email.com',
+  password: 'susanpassword',
+}
+
 test('Should create a user', async () => {
   await request(app)
     .post('/users/signup')
-    .send(matt)
+    .send(userOne)
     .then((res) => {
       expect(res.statusCode).toBe(201)
     })
 })
 
 test("Should verify the new User's data", async () => {
-  await request(app)
-    .post('/users/signup')
-    .send(john)
-    .then((res) => {
-      expect(res.body.name).toBe('John')
-      expect(res.body.email).toBe('john@email.com')
-      expect(res.body.password).toBe('johnpassword')
-      expect(res.body._id).toBeDefined()
-      expect(res.body.joined).toBeDefined()
-    })
+  const newUser = await request(app).post('/users/signup').send(userTwo)
+
+  expect(newUser.body.name).toBe('John')
+  expect(newUser.body.email).toBe('john@email.com')
+  expect(newUser.body.password).toBe('johnpassword')
+  expect(newUser.body._id).toBeDefined()
+  expect(newUser.body.joined).toBeDefined()
 })
 
 test('Should delete a user', async () => {
   // create a User named susan and save it to the db
-  const susan = new User({
-    name: 'Susan',
-    email: 'susan@email.com',
-    password: 'susanpassword',
-  })
+  const susan = new User(userThree)
   await susan.save()
   let susanID = susan._id.toString()
 
@@ -60,4 +59,22 @@ test('Should delete a user', async () => {
   // confirm record has been deleted
   const deletedUser = await User.findById(susanID)
   expect(deletedUser).toBe(null)
+})
+
+test('Should update a user name', async () => {
+  // create and save a user named matt
+  const matt = new User(userOne)
+  await matt.save()
+  let mattID = matt._id.toString()
+
+  // update the user's name using the PATCH method
+  await request(app)
+    .patch(`/users/${mattID}`) // <- do not use '/users/:id' here!! :O
+    .send({ name: 'Matteo', email: 'matteo@email.com' })
+    .expect(200)
+
+  // find a user with matt's previously saved id
+  const updatedUser = await User.findById(mattID)
+
+  expect(updatedUser.name).toBe('Matteo')
 })
