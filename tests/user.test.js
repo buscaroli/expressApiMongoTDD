0858f0bef4c904bc0f6fb654b9ae9f75c7832280
mabus <m.buscaroli@outlook.com>
2022-03-23 10:35:48 +0000

    fixed jest call in package.json and deleting all users from database before testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,17 +1,27 @@
 const request = require('supertest')
 const app = require('../src/app')
 const mongoose = require('mongoose')
+const User = require('../src/models/user')
 
-afterAll(() => mongoose.connection.close())
+beforeEach(async () => await User.deleteMany())
+afterAll(async () => await mongoose.connection.close())
+
+const matt = {
+  name: 'Matt',
+  email: 'matt@email.com',
+  password: 'mattpassword',
+}
+
+const john = {
+  name: 'John',
+  email: 'john@email.com',
+  password: 'johnpassword',
+}
 
 test('creates an user', async () => {
   await request(app)
     .post('/users/signup')
-    .send({
-      name: 'Matt',
-      email: 'matt@email.com',
-      password: 'mattpassword',
-    })
+    .send(matt)
     .then((res) => {
       expect(res.statusCode).toBe(201)
     })
