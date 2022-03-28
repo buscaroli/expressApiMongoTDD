59a1e253f64907cfbe63e7992b8ed66000a4006e
mabus <m.buscaroli@outlook.com>
2022-03-28 15:07:42 +0100

    added a function to get the date in US format

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -8,6 +8,16 @@ const today = () => {
   return nowString
 }
 
+const todayUS = () => {
+  let now = new Date()
+  let day = addPadding(now.getDate())
+  let month = addPadding((1 + now.getMonth()).toString())
+  let year = now.getFullYear()
+
+  let nowString = `${month}-${day}-${year}`
+  return nowString
+}
+
 const addPadding = (value) => {
   if (value.length === 1) {
     return `0${value}`
@@ -16,4 +26,4 @@ const addPadding = (value) => {
   }
 }
 
-module.exports = { today, addPadding }
+module.exports = { today, todayUS, addPadding }
